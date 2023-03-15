81df293713ab72078ddb435c7993ec88f77ccf55
Nodirbek Ergashev <mr.nodirbek77@gmail.com>
2023-03-15 15:07:47 +0500

    feat: Post controller

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { PostsController } from './controllers/posts/posts.controller';
 
-@Module({})
+@Module({
+  controllers: [PostsController],
+})
 export class PostsModule {}
